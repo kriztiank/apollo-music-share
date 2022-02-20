37a3d84f8b91e43025e9b370824c583821c83ffd
kriztiank <kriztiank@gmail.com>
2022-02-20 10:10:39 +0100

    final ui created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
-import { Grid } from '@material-ui/core'
 import React from 'react'
 import AddSong from './components/AddSong'
 import Header from './components/Header'
 import SongList from './components/SongList'
 import SongPlayer from './components/SongPlayer'
+import { Grid, useMediaQuery, Hidden } from '@material-ui/core'
 
 function App() {
+  const greatherThanSm = useMediaQuery((theme) => theme.breakpoints.up('sm'))
+  const greatherThanMd = useMediaQuery((theme) => theme.breakpoints.up('md'))
+
   return (
     <>
-      <Header />
+      <Hidden only='xs'>
+        <Header />
+      </Hidden>
       <Grid container spacing={3}>
-        <Grid  style={{paddingTop: 80}} item xs={12} md={7}>
+        {/* AddSong + SongList */}
+        <Grid
+          style={{ paddingTop: greatherThanSm ? 80 : 10 }}
+          item
+          xs={12}
+          md={7}
+        >
           <AddSong />
           <SongList />
         </Grid>
-        <Grid item xs={12} md={5}>
+        {/* SongPlayer */}
+        <Grid
+          style={
+            greatherThanMd
+              ? {
+                  position: 'fixed',
+                  width: '100%',
+                  right: 0,
+                  top: 70,
+                }
+              : {
+                  position: 'fixed',
+                  width: '100%',
+                  left: 0,
+                  bottom: 0,
+                }
+          }
+          item
+          xs={12}
+          md={5}
+        >
           <SongPlayer />
         </Grid>
       </Grid>
--- src/components/QueuedSongList.js
+++ src/components/QueuedSongList.js
@@ -1,7 +1,80 @@
 import React from 'react'
+import {
+  Avatar,
+  IconButton,
+  Typography,
+  makeStyles,
+  useMediaQuery,
+} from '@material-ui/core'
+import { Delete } from '@material-ui/icons'
 
 export default function QueuedSongList() {
+  const greatherThanMd = useMediaQuery(theme => theme.breakpoints.up('md'))
+
+  const song = {
+    title: 'MÖÖN',
+    artist: 'LÜNE',
+    thumbnail: 'https://img.youtube.com/vi/--ZtUFsIgMk/0.jpg',
+  }
+
+  return greatherThanMd && (
+    <div style={{ margin: '10px 0' }}>
+      <Typography color='textSecondary' variant='button'>
+        QUEUE (5)
+      </Typography>
+      {Array.from({ length: 5 }, () => song).map((song, i) => (
+        <QueuedSong key={i} song={song} />
+      ))}
+    </div>
+  )
+}
+
+// makeStyles no theme
+const useStyles = makeStyles({
+  container: {
+    display: 'grid',
+    gridAutoFlow: 'column',
+    gridTemplateColumns: '50px auto 50px',
+    gridGap: 12,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  avatar: {
+    width: 44,
+    height: 44,
+  },
+  songInfoContainer: {
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+  },
+  text: {
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+  },
+})
+
+function QueuedSong({ song }) {
+  const classes = useStyles()
+  const { thumbnail, artist, title } = song
+
   return (
-    <div>QueuedSongList</div>
+    <div className={classes.container}>
+      <Avatar className={classes.avatar} src={thumbnail} alt='Song thumbnail' />
+      <div className={classes.songInfoContainer}>
+        <Typography className={classes.text} variant='subtitle2'>
+          {title}
+        </Typography>
+        <Typography
+          className={classes.text}
+          color='textSecondary'
+          variant='body2'
+        >
+          {artist}
+        </Typography>
+      </div>
+      <IconButton>
+        <Delete color='error' />
+      </IconButton>
+    </div>
   )
 }
